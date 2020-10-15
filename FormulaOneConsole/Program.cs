cf1ce2a9c5a8bf14af78e45d0434d9cca48ed076
IncubuSpirit <stefirca.alex.as@gmail.com>
2020-10-15 13:20:18 +0200

    gestione db avanzata fino al reset, da mettere a posto il resto

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
-
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace FormulaOneConsole
 {
     class Program
     {
-        public const string WORKINGPATH = @"D:\FormulaOne_data";
+        public const string WORKINGPATH = @"D:\FormulaOne_data\";
         private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
 
         private static void menu()
         {
             Console.Clear();
             Console.Title = "Formula one - Batch operations";
-            Console.WriteLine("*** Formula one - Batch operations ***");
-            Console.WriteLine("1 - Execute import countries table;");
-            Console.WriteLine("2 - Execute import teams table;");
-            Console.WriteLine("3 - Execute import drivers table;");
+            Console.WriteLine("###    Formula one - Batch operations    ###");
+            Console.WriteLine("1 - Create Countries table;");
+            Console.WriteLine("2 - Create Teams table;");
+            Console.WriteLine("3 - Create Drivers table;");
+            Console.WriteLine(" - - - - - - - - - - - - -");
+            Console.WriteLine("R - Reset DataBase;");
+            Console.WriteLine(" - - - - - - - - - - - - -");
             Console.WriteLine("X - Exit.");
-            Console.Write("\nYour selection: ");
+            Console.Write("\n_");
         }
 
         static void Main(string[] args)
@@ -28,16 +32,25 @@ namespace FormulaOneConsole
             do
             {
                 menu();
-                c = Console.ReadKey().KeyChar;
+                c = Char.ToUpper(Console.ReadKey().KeyChar);
                 switch (c)
                 {
                     case '1':
                         ExecuteSqlScript("Countries.sql");
                         break;
+                    case '2':
+                        ExecuteSqlScript("Teams.sql");
+                        break;
+                    case '3':
+                        ExecuteSqlScript("Drivers.sql");
+                        break;
+                    case 'R':
+                        ResetDb();
+                        break;
                     default:
                         if (c != 'x' && c != 'X')
                         {
-                            Console.WriteLine("Effettua un'altra selezione.");
+                            Console.WriteLine("Invalid selection.");
                         }
                         break;
                 }
@@ -46,19 +59,15 @@ namespace FormulaOneConsole
 
         public static void ExecuteSqlScript(string sqlScriptName)
         {
-            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
-            fileContent = fileContent
-                .Replace("\r\n", "")
-                .Replace("\r", "")
-                .Replace("\n", "")
-                .Replace("\t", "");
+            string fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace(",", "\n");
             var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
 
-            var con = new SqlConnection(CONNECTION_STRING);
-            var cmd = new SqlCommand("query", con);
+            SqlConnection con = new SqlConnection(CONNECTION_STRING);
+            SqlCommand cmd = new SqlCommand("query", con);
             con.Open();
-            var i = 0;
-            int nr = 0;
+
+            int i = 0, nr = 0;
             foreach (var query in sqlqueries)
             {
                 cmd.CommandText = query;
@@ -74,10 +83,25 @@ namespace FormulaOneConsole
                     nr++;
                 }
             }
+
             Console.Clear();
             Console.WriteLine($"Processo di creazione della tabella \"{sqlScriptName.Substring(0, sqlScriptName.IndexOf('.'))}\" terminato con {i} error{(i == 1 ? "e" : "i")}.");
-            System.Threading.Thread.Sleep(3500);
+            System.Threading.Thread.Sleep(3000);
             con.Close();
+        }
+        public static void ResetDb()
+        {
+            string[] tables = new string[] { "Countries.sql", "Teams.sql", "drivers.sql"};
+            for (int i = 0; i < tables.Length; i++)
+            {
+                SqlConnection con = new SqlConnection(CONNECTION_STRING);
+                SqlCommand cmd = new SqlCommand("query", con);
+                con.Open();
+                string query = "DROP TABLE IF EXIST"+tables[i];
+                cmd.CommandText = query;
+                con.Close();
+                ExecuteSqlScript(tables[i]);
+            }
         }
     }
 }
